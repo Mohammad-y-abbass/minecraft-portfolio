2cc151f842cfe510095cb9a5ae946ea72735be15
Mohammad Abbass <144027220+Mohammad-y-abbass@users.noreply.github.com>
2026-02-02 13:18:26 +0200

    feat: add cubes

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,62 @@
 import * as THREE from "three";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x80a0e0);
 document.body.appendChild(renderer.domElement);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(0, 0, 0);
+camera.position.set(-32, 16, -32);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(16, 0, 16);
+controls.update();
 
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
+function setupLights() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1, 1, 1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1, -1, -0.5);
+  scene.add(light2);
+
+  const light3 = new THREE.AmbientLight();
+  light3.intensity = 0.1;
+  scene.add(light3);
+}
+
+function setupWorld() {
+  for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < 32; j++) {
+
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.set(i, 0, j)
+      scene.add(cube);
+    }
+  }
+}
+
+
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 
@@ -29,5 +65,6 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
-
+setupLights();
+setupWorld();
 animate();
